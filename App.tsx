047d7c908fab415033a4b7a5118963028e3d2474
Glen Adamson <glen.adamson@terraquest.co.uk>
2022-02-04 16:24:09 +0000

    husky works

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 let x = 0;
-
 export default function App() {
   return (
     <View style={styles.container}>
